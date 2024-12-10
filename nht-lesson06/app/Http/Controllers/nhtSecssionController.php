7767905b268ebfc51a36390e3c4e7025d3fa8df6
unknown <bhuythong04@gmail.com>
2024-12-10 23:31:40 +0700

    nht-lesson06

--- nht-lesson06/app/Http/Controllers/nhtSecssionController.php
+++ nht-lesson06/app/Http/Controllers/nhtSecssionController.php
@@ -3,28 +3,29 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 class SessionController extends Controller
 {
-    #Đọc dữ liệu từ session
-        public function getSessionData(Request $request)
+    //Đọc dữ liệu từ session
+        public function nhtgetSessionData(Request $request)
         {
             if($request->session()->has('K23CNT2_NHT'))
             {
-                echo $request->session()->get('K23CNT2_NHT');
+                echo "<h2> Session Data:". $request->session()->get("K23CNT2_NHT");
             }
             else
             {
                 echo "<h2> Không có dữ liệu trong session </h2>";
             }
         }
-    #Lưu dữ liệu vào session
-        public function storeSessionData(Request $request)
+    //Ghi dữ liệu vào session
+        public function nhtstoreSessionData(Request $request)
         {
-            $request->session()->put('name','K23CNT2_NHT');
+            //lưu 
+            $request->session()->put('K23CNT2_NHT','K23CNT2 - Nguyễn Huy Thông - 2310900102');
             echo "<h2> Dữ liệu đã được lưu và session </h2>";
         }
     #Xóa dữ liệu trong session
         public function deleteSessionData(Request $request)
         {
-                $request->session()->forget('K23CNT2_NHT');
-                echo "<h2> Dữ liệu đã được xóa khỏi session </h2>";
+            $request->session()->forget('K23CNT2_NHT');
+            echo "<h2> Dữ liệu đã được xóa khỏi session </h2>";
         }
 }
--- nht-lesson06/routes/web.php
+++ nht-lesson06/routes/web.php
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//test seesson
 route::get('nhtSecssionController/get',[nhtSecssionController::class,'getSecssionController'])->name('nhtSecssionController.get');
 route::get('nhtSecssionController/set',[nhtSecssionController::class,'setSecssionController'])->name('nhtSecssionController.set');
 route::get('nhtSecssionController/delete',[nhtSecssionController::class,'deleteSecssionController'])->name('nhtSecssionController.delete');
